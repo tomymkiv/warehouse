b110fa687addf56e2a355a2f3b99208daa98b09e
Tomas <liberfran.04@gmail.com>
2025-12-26 18:37:07 -0300

    Set files

---
+++ app/Models/Products.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductsFactory> */
+    use HasFactory;
+    protected $fillable = ['name', 'description', 'stock', 'price'];
+    // campos que puedo modificar en un formulario (asignacion masiva)
+}
---
+++ database/migrations/2025_12_26_211141_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedInteger('stock')->default(0); // el valor por default es 0
+            $table->decimal('price', 8, 2)->default(0); // 8: cantidad maximo de digitos. 2: cantidad de decimales
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
